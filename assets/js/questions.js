fda0838391bb956b0843508753df4e846dba025c
Alex Standfield <AlexStandfield@gmail.com>
2022-06-14 15:26:59 -0600

    fully functional

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,3 @@
-
-
 // Timer
 let timeleft = 75;
 
@@ -12,3 +10,44 @@ let downloadTimer = setInterval(function(){
     timer.textContent = 0 + timeleft;
     timeleft -= 1;
 }, 1000);
+
+let getQuestions = function () {
+    if (i <=4) {
+        question.textContent = questionsArr[i].question
+        answer1.textContent = questionsArr[i].correctAnswer
+        answer2.textContent = questionsArr[i].a1
+        answer3.textContent = questionsArr[i].a2
+        answer4.textContent = questionsArr[i].a3
+    } else {
+        finishQuizBox.style.display = 'flex';
+    }
+};
+
+let answerQuestion = function () {
+    if (this.textContent === questionsArr[i].correctAnswer) {
+        rightOrWrong.textContent = 'Correct!'
+        score++;
+        localStorage.setItem("myScore", JSON.stringify(score));
+        console.log(localStorage.getItem('myScore'));
+        i++;
+        getQuestions();
+    } else {
+        rightOrWrong.textContent = 'Wrong!'
+        i++;
+        getQuestions();
+    }
+};
+
+let resetScore = function () {
+    score = 0;
+    i = 0;
+    localStorage.setItem("myScore", JSON.stringify(score));
+};
+
+resetScore();
+getQuestions();
+
+answer1.addEventListener('click', answerQuestion);
+answer2.addEventListener('click', answerQuestion);
+answer3.addEventListener('click', answerQuestion);
+answer4.addEventListener('click', answerQuestion);
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ let answer3 = document.querySelector('#answer-3');
 let answer4 = document.querySelector('#answer-4');
 let timer = document.querySelector('#timer');
 let finishQuizBox = document.querySelector('#finish-quiz-box');
+let finishQuiz = document.querySelector('#finish-quiz')
+
+let finalScore = document.querySelector('#final-score');
 
 let answerButton = document.querySelector('.btn');
 
@@ -17,69 +20,37 @@ let score = 0;
 let questionsArr = [
     {
         question: 'Commonly used data types DO NOT include:',
-        correctAnswer: 'alerts',
-        a1: 'strings',
-        a2: 'booleans',
-        a3: 'numbers'
+        correctAnswer: '1. alerts',
+        a1: '2. strings',
+        a2: '3. booleans',
+        a3: '4. numbers'
     },
     {
         question: 'The condition of an if/else statement is enclosed within ____.',
-        correctAnswer: 'curley brackets',
-        a1: 'qoutes',
-        a2: 'parentheses',
-        a3: 'square bracket'
+        correctAnswer: '1. curley brackets',
+        a1: '2. qoutes',
+        a2: '3. parentheses',
+        a3: '4. square bracket'
     },
     {
         question: 'Arrays in Javascript can be used to store ____.',
-        correctAnswer: 'all of the above',
-        a1: 'numbers and strings',
-        a2: 'other arrays',
-        a3: 'booleans'
+        correctAnswer: '1. all of the below',
+        a1: '2. numbers and strings',
+        a2: '3. other arrays',
+        a3: '4. booleans'
     },
     {
         question: 'String values must be enclosed within ____ when being assigned to variables.',
-        correctAnswer: 'qoutes',
-        a1: 'commas',
-        a2: 'curly brackets',
-        a3: 'parentheses'
+        correctAnswer: '1. qoutes',
+        a1: '2. commas',
+        a2: '3. curly brackets',
+        a3: '4. parentheses'
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-        correctAnswer: 'console.log',
-        a1: 'Javascript',
-        a2: 'terminal/bash',
-        a3: 'for loops'
+        correctAnswer: '1. console.log',
+        a1: '2. Javascript',
+        a2: '3. terminal/bash',
+        a3: '4. for loops'
     }
 ];
-
-let getQuestions = function () {
-    if (i <=4) {
-        question.textContent = questionsArr[i].question
-        answer1.textContent = questionsArr[i].correctAnswer
-        answer2.textContent = questionsArr[i].a1
-        answer3.textContent = questionsArr[i].a2
-        answer4.textContent = questionsArr[i].a3
-    } else {
-        finishQuizBox.style.display = 'flex';
-    }
-};
-
-let answerQuestion = function () {
-    if (this.textContent === questionsArr[i].correctAnswer) {
-        rightOrWrong.textContent = 'Correct!'
-        score++;
-        i++;
-        getQuestions();
-    } else {
-        rightOrWrong.textContent = 'Wrong!'
-        i++;
-        getQuestions();
-    }
-};
-
-getQuestions();
-
-answer1.addEventListener('click', answerQuestion);
-answer2.addEventListener('click', answerQuestion);
-answer3.addEventListener('click', answerQuestion);
-answer4.addEventListener('click', answerQuestion);
